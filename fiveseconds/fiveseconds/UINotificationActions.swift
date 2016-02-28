81ba24719e33ea8685aa840ef2ac4b16fdc6e93a
Brandon Kirklen <brandonkirklen@gmail.com>
2016-02-28 08:54:18 -0800

    UINotification Changes

--- fiveseconds/fiveseconds/UINotificationActions.swift
+++ fiveseconds/fiveseconds/UINotificationActions.swift
@@ -10,44 +10,30 @@ import Foundation
 import UIKit
 
 func UINotificationActionInit() {
-    // increment Action
-    let incrementAction = UIMutableUserNotificationAction()
-    incrementAction.identifier = "INCREMENT_ACTION"
-    incrementAction.title = "Add +1"
-    incrementAction.activationMode = UIUserNotificationActivationMode.Background
-    incrementAction.authenticationRequired = true
-    incrementAction.destructive = false
+    // yes Action
+    let yesAction = UIMutableUserNotificationAction()
+    yesAction.identifier = "YES_ACTION"
+    yesAction.title = "Yes"
+    yesAction.activationMode = UIUserNotificationActivationMode.Background
+    yesAction.authenticationRequired = true
+    yesAction.destructive = false
     
-    // decrement Action
-    let decrementAction = UIMutableUserNotificationAction()
-    decrementAction.identifier = "c"
-    decrementAction.title = "Sub -1"
-    decrementAction.activationMode = UIUserNotificationActivationMode.Background
-    decrementAction.authenticationRequired = true
-    decrementAction.destructive = false
-    
-    // reset Action
-    let resetAction = UIMutableUserNotificationAction()
-    resetAction.identifier = "RESET_ACTION"
-    resetAction.title = "Reset"
-    resetAction.activationMode = UIUserNotificationActivationMode.Foreground
-    // NOT USED resetAction.authenticationRequired = true
-    resetAction.destructive = true
-    
+    // no Action
+    let noAction = UIMutableUserNotificationAction()
+    noAction.identifier = "NO_ACTION"
+    noAction.title = "No"
+    noAction.activationMode = UIUserNotificationActivationMode.Background
+    noAction.authenticationRequired = true
+    noAction.destructive = false
     
     // Category
-    let counterCategory = UIMutableUserNotificationCategory()
-    counterCategory.identifier = "COUNTER_CATEGORY"
-    
-    // A. Set actions for the default context
-    counterCategory.setActions([incrementAction, decrementAction, resetAction],
-        forContext: UIUserNotificationActionContext.Default)
-    
-    // B. Set actions for the minimal context
-    counterCategory.setActions([incrementAction, decrementAction],
+    let questionCategory = UIMutableUserNotificationCategory()
+    questionCategory.identifier = "QUESTION_CATEGORY"
+
+    questionCategory.setActions([yesAction, noAction],
         forContext: UIUserNotificationActionContext.Minimal)
     
-    let settings = UIUserNotificationSettings(forTypes: [.Alert, .Sound], categories: NSSet(object: counterCategory) as? Set<UIUserNotificationCategory>)
+    let settings = UIUserNotificationSettings(forTypes: [.Alert, .Sound], categories: [questionCategory])
     UIApplication.sharedApplication().registerUserNotificationSettings(settings)
 
 }
